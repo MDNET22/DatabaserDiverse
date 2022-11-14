c299423b7098788c478e64b3feb3c854c221ddf7
MDNET22 <mathias.damgaard@iths.se>
2022-11-14 14:30:24 +0100

    Update MathiasIntro.sql
    Update file

--- SQLÖvningarMathias/MathiasIntro.sql
+++ SQLÖvningarMathias/MathiasIntro.sql
@@ -1,10 +1,31 @@
-CREATE DATABASE DjurDb;
+--CREATE DATABASE DjurDb;
 
-USE DjurDb;
+--CREATE DATABASE KlassenDb;
+--USE DjurDb;
 
---CREATE TABLE DJURTbl (
+--CREATE TABLE DjurTbl (
 --	[ID] int Primary Key,
 --	[Namn] nvarchar(max) not null
 --);
 
-SELECT * FROM DjurDb;
+--INSERT INTO DjurTbl ([Id], [Namn])
+--VALUES (4, 'Kanin'); 
+
+--SELECT * FROM DjurTbl
+--WHERE Namn LIKE '%a%';
+
+--DROP DATABASE DjurDb;
+
+--CREATE TABLE PersonTbl (
+--	[ID] int Primary Key,
+--	[Förnamn] nvarchar(max) not null,
+--	[Efternamn] nvarchar(max) not null,
+--	[Ålder] int,
+--	[Hemort] nvarchar(max) not null
+--);
+
+
+--INSERT INTO PersonTbl ([Id], [Förnamn], [Efternamn], [Ålder], [Hemort])
+--VALUES (5, 'Farid', 'Afradi', 33, 'Göteborg');
+
+SELECT * FROM PersonTbl;
