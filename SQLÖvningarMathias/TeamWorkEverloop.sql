1e8272f385a9e37aa1ad474f23cc176553ac9d3d
MDNET22 <mathias.damgaard@iths.se>
2022-11-16 11:54:28 +0100

    Update TeamWorkEverloop.sql
    Uppgift 3 klar

--- SQLÖvningarMathias/TeamWorkEverloop.sql
+++ SQLÖvningarMathias/TeamWorkEverloop.sql
@@ -7,7 +7,7 @@
 --select Title, CONCAT(FORMAT(Season, 'S0#') ,FORMAT(EpisodeInSeason, 'E0#')) as Episode
 --FROM GameOfThrones;
 
-select * from Users2;
+--select * from Users2;
 
 --SELECT LOWER(CONCAT(SUBSTRING(FirstName, 1, 2), SUBSTRING(LastName, 1, 2))) AS UserName
 --FROM Users2;
@@ -15,4 +15,17 @@ select * from Users2;
 --update Users2
 --set UserName = LOWER(CONCAT(SUBSTRING(FirstName, 1, 2),
 --SUBSTRING(LastName, 1, 2)));
+
+--select * from Airports2;
+
+--update Airports2
+--set Time = '-'
+--where Time IS NULL;
+
+
+--update Airports2
+--set DST = '-'
+--where DST IS NULL;
+
+
 
