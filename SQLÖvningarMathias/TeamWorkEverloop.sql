9ba6f104f2313f9ec78615d923df7fef4e953a32
MDNET22 <mathias.damgaard@iths.se>
2022-11-16 11:32:51 +0100

    Update TeamWorkEverloop.sql
    Uppgift 1 ej klar
    Uppgift 2 klar

---
+++ SQLÖvningarMathias/TeamWorkEverloop.sql
@@ -0,0 +1,18 @@
+--use everyloop;
+
+
+--select * from GameOfThrones;
+
+
+--select Title, CONCAT(FORMAT(Season, 'S0#') ,FORMAT(EpisodeInSeason, 'E0#')) as Episode
+--FROM GameOfThrones;
+
+select * from Users2;
+
+--SELECT LOWER(CONCAT(SUBSTRING(FirstName, 1, 2), SUBSTRING(LastName, 1, 2))) AS UserName
+--FROM Users2;
+
+--update Users2
+--set UserName = LOWER(CONCAT(SUBSTRING(FirstName, 1, 2),
+--SUBSTRING(LastName, 1, 2)));
+
