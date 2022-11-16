60caaa9a7bf8c5be92a8a94afe37e39ed12f74b1
MDNET22 <mathias.damgaard@iths.se>
2022-11-16 13:00:19 +0100

    Update TeamWorkEverloop.sql
    Fråga 4

--- SQLÖvningarMathias/TeamWorkEverloop.sql
+++ SQLÖvningarMathias/TeamWorkEverloop.sql
@@ -27,5 +27,23 @@
 --set DST = '-'
 --where DST IS NULL;
 
+--Ta bort de rader från (kopia på) tabellen Elements där ”Name” är någon av följande:
+--'Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium', samt alla rader där ”Name” börjar
+--på någon av bokstäverna d, k, m, o, eller u.
 
+--delete from Elements2
+--where Name IN ('Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium')
+--and Name IN ('d%', 'k%', 'm%', 'o%', 'u%'); 
 
+--DELETE FROM Elements2
+-- WHERE [Name] IN ('Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium') or
+-- [Name] LIKE 'D%' or [name] like 'K%' or [name] like 'M%' or [name] like 'O%' or [name] like 'U%';
+
+--select * from Elements3;
+
+--delete from Elements3
+--where name = 'D%' and name = 'K%' and name = 'M%' and name = 'O%' and name = 'U%';
+
+ --DELETE FROM Elements3
+ --WHERE [Name] IN ('Erbium', 'Helium', 'Nitrogen', 'Platinum', 'Selenium') or
+ --[Name] LIKE 'd%' or [name] like 'k%' or [name] like 'm%' or [name] like 'o%' or [name] like'u%';
