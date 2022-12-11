da9ca3e4484eb21c284a1b4830bcb5112c64ec42
MDNET22 <mathias.damgaard@iths.se>
2022-12-11 19:06:27 +0100

    KeysDemoDb CS Console Application

--- SQLÖvningarMathias/KeysDemoDb/KeysDemoDb/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using KeysDemoDb.Models;
-
-Console.WriteLine("Hello, World!");
-
-var KeysDemoContext = new KeysDemoDbContext();
-
-KeysDemoContext.SaveChanges();
