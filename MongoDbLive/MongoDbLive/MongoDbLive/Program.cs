56f266278d6f643748142470225a90f97045c9ab
MDNET22 <mathias.damgaard@iths.se>
2023-01-12 09:53:48 +0100

    Push

---
+++ MongoDbLive/MongoDbLive/MongoDbLive/Program.cs
@@ -0,0 +1,59 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+var hostname = "localhost";
+var port = 27017;
+var databaseName = "liveDemo";
+var connectionString = $"mongodb://{hostname}:{port}";
+
+var client = new MongoClient(connectionString);
+var database = client.GetDatabase(databaseName);
+
+var collection = database.GetCollection<Person>(
+    "people",
+    new MongoCollectionSettings()
+    {
+        AssignIdOnInsert = true
+    });
+
+//var Thilde = new Person()
+//{
+//    Age = 6, 
+//    FirstName = "Thilde", 
+//    Lastname = "Hjelm"
+//};
+
+//collection.InsertOne(Thilde);
+
+var person = collection.Find(p=>p.FirstName.Equals("Niklas")).ToList();
+
+//foreach (var person in people)
+//{
+//    Console.WriteLine(person.FirstName);
+//}
+
+var filter = Builders<Person>.Filter.Eq("FirstName", "Niklas");
+var update = Builders<Person>.Update.Set("Age", 36);
+
+collection.FindOneAndUpdate(filter, update);
+
+person = collection.Find(p => p.FirstName.Equals("Niklas")).ToList();
+
+Console.WriteLine(person);
+
+
+
+record Person
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement] public string FirstName { get; set; } = string.Empty;
+
+    [BsonElement] public string Lastname { get; set; } = string.Empty;
+
+    [BsonElement]
+    public int Age { get; set; }
+
+}
---
+++ MongoDbLive/MongoDbövning/MongoDbövning/Program.cs
@@ -0,0 +1,71 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+var hostname = "localhost";
+var port = 27017;
+var databaseName = "liveDemoRep";
+var connectionString = $"mongodb://{hostname}:{port}";
+
+var client = new MongoClient(connectionString);
+var database = client.GetDatabase(databaseName);
+
+var collection = database.GetCollection<Car>(
+    "Vehicles",
+    new MongoCollectionSettings()
+    {
+        AssignIdOnInsert = true
+    });
+
+//var Volvo = new Car()
+//{
+//    Brand = "Volvo",
+//    Model = "XC90",
+//    FuelType = "Petrol"
+//};
+
+//var BMW = new Car()
+//{
+//    Brand = "BMW",
+//    Model = "XC90",
+//    FuelType = "Diesel"
+//};
+
+//var WW = new Car()
+//{
+//    Brand = "WW",
+//    Model = "Golf",
+//    FuelType = "Petrol"
+//};
+
+//var Tesla = new Car()
+//{
+//    Brand = "Tesla",
+//    Model = "Model 3",
+//    FuelType = "Electric"
+//};
+
+//collection.InsertOne(Volvo);
+//collection.InsertOne(BMW);
+//collection.InsertOne(WW);
+//collection.InsertOne(Tesla);
+
+var car = collection.Find(c => c.Model.Equals("Niklas")).ToList();
+
+Console.WriteLine(car);
+
+class Car
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement] public string Brand { get; set; } = string.Empty;
+
+    [BsonElement] public string Model { get; set; } = string.Empty;
+
+    [BsonElement] public string FuelType { get; set; } = string.Empty;
+
+    //[BsonElement]
+    //public int Age { get; set; }
+}
